f5596f6a5f53090771354e076d63011b6be55d7f
Peter Horton <phorton@softstackfactory.com>
2019-12-07 21:24:57 +0000

    initial commit

--- index.js
+++ index.js
@@ -5,35 +5,8 @@
 // (You can remove or add an extra slash to the top @ts-check line if you want things to be a bit less strict)
 
 /** This function runs as soon as the p5 library loads */
-function setup() {
-  createCanvas(400, 600);
-  // set up some settings to make everything centered by default
-  textAlign(CENTER, CENTER);
-  rectMode(CENTER);
+function hello (){
+    alert("Hello")
 }
 
-/** This function runs in an infinitely repeating loop */
-function draw() {
-  if (mouseIsPressed) {
-    fill("black");
-    stroke("white");
-  } else {
-    fill("white");
-    stroke("black");
-  }
-
-  ellipse(mouseX, mouseY, 80, 80);
-
-  writeText();
-}
-/* This function draws our welcome text and its rectangle background */
-function writeText() {
-  fill("pink");
-  stroke("black");
-  rect(width / 2, 50, 300, 50);
-  fill("black");
-  stroke("black");
-  textSize(32);
-  text("Welcome to p5js!", width / 2, 50);
-
-}
+hello()
